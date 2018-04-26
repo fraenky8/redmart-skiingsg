fc976e573e8e7a382eaca5d7f39482b215bdb657
Frank Meyer <fr4nk235@gmail.com>
2018-04-26 20:07:30 +0200

    ready to publish

---
+++ README.md
@@ -0,0 +1,10 @@
+### Skiing in Singapore
+
+Possible solution of the [coding puzzle](http://geeks.redmart.com/2015/01/07/skiing-in-singapore-a-coding-diversion/) of [redmart](https://github.com/Redmart).
+
+##### Usage
+from `bin` folder:
+
+``
+java -jar redmart-skiingsg.jar ../resources/redmart/skiingsg/map.txt
+``
--- src/com/redmart/skiingsg/Main.java
+++ src/com/redmart/skiingsg/Main.java
@@ -18,6 +18,9 @@ public class Main
         int row;
         int col;
 
+        LinkedList<Node> neighbours = new LinkedList<>();
+        boolean isSource = false;
+
         Node(int value, int row, int col)
         {
             this.value = value;
@@ -29,6 +32,14 @@ public class Main
         public String toString()
         {
             return String.format("%d", value);
+        }
+
+        String print()
+        {
+            return String.format(
+                    "{%d, isSource: %b, \tNeighours: %s}",
+                    value, isSource, neighbours
+            );
         }
     }
 
@@ -56,6 +67,8 @@ public class Main
 
     private static List<Route> routes = new LinkedList<>();
 
+    private static final boolean COLLECT_ALL_LONGEST_ROUTES = false;
+
     public static void main(String[] args)
     {
         if (args.length != 1)
@@ -73,94 +86,75 @@ public class Main
         try
         {
             graph = main.readMapFile(mapFile);
+            main.addNeighbours(graph);
         }
         catch (IOException e)
         {
             System.out.printf("error processing file '%s':\n\n", mapFile);
             e.printStackTrace();
+            return;
         }
 
-        main.dfs(graph);
+        main.dfsRecursiveAL(graph);
 
-        System.out.printf(
-            "\nfound %d route(s) with length %d and steep %d:\n\n",
-            routes.size(), routes.get(0).size(), routes.get(0).steep
-        );
-        for (Route route : routes)
-        {
-            System.out.print(
-                route.stream()
-                    .map(n -> n.value)
-                    .sorted(Collections.reverseOrder())
-                    .map(Object::toString)
-                    .collect(Collectors.joining(" -> "))
-            );
-        }
-        System.out.println();
+        print(routes);
     }
 
-    private void dfs(Node[][] graph)
+    private void dfsRecursiveAL(Node[][] graph)
     {
+        routes.clear();
+
+        List<Node> visited = new ArrayList<>(graph.length);
+        Map<Node, Node> parents = new LinkedHashMap<>();
+
         for (Node[] nodes : graph)
         {
             for (Node node : nodes)
             {
-                Map<Node, Node> parents = new HashMap<>();
-                visit(node, graph, parents);
-                findRoutes(parents);
+                if (!node.isSource)
+                { continue; }
+
+                visited.clear();
+                parents.clear();
+
+                visitRecursiveAL(node, parents, visited);
+                collectRoutes(parents);
             }
         }
     }
 
-    private void visit(Node node, Node[][] graph, Map<Node, Node> parents)
+    private void visitRecursiveAL(Node node, Map<Node, Node> parents, List<Node> visited)
     {
-        boolean north = node.row - 1 >= 0 && graph[node.row - 1][node.col].value < node.value;
-        if (north)
-        {
-            parents.put(graph[node.row - 1][node.col], node);
-            visit(graph[node.row - 1][node.col], graph, parents);
-        }
+        visited.add(node);
 
-        boolean east = node.col + 1 < graph[node.row].length && graph[node.row][node.col + 1].value < node.value;
-        if (east)
+        for (Node n : node.neighbours)
         {
-            parents.put(graph[node.row][node.col + 1], node);
-            visit(graph[node.row][node.col + 1], graph, parents);
-        }
+            if (visited.contains(n))
+            { continue; }
 
-        boolean south = node.row + 1 < graph.length && graph[node.row + 1][node.col].value < node.value;
-        if (south)
-        {
-            parents.put(graph[node.row + 1][node.col], node);
-            visit(graph[node.row + 1][node.col], graph, parents);
-        }
-
-        boolean west = node.col - 1 >= 0 && graph[node.row][node.col - 1].value < node.value;
-        if (west)
-        {
-            parents.put(graph[node.row][node.col - 1], node);
-            visit(graph[node.row][node.col - 1], graph, parents);
+            parents.put(n, node);
+            visitRecursiveAL(n, parents, visited);
         }
     }
 
-    private void findRoutes(Map<Node, Node> parents)
+    private void collectRoutes(Map<Node, Node> parents)
     {
         for (Map.Entry<Node, Node> entry : parents.entrySet())
         {
             LinkedList<Node> route = new LinkedList<>();
             route.add(entry.getKey());
-            findRoutes(entry.getKey(), parents, route);
+            collectRoutes(entry.getKey(), parents, route);
         }
     }
 
-    private void findRoutes(Node node, Map<Node, Node> parents, LinkedList<Node> route)
+    private void collectRoutes(Node node, Map<Node, Node> parents, LinkedList<Node> route)
     {
         Node parent = parents.get(node);
 
         if (parent != null)
         {
             route.add(parent);
-            findRoutes(parent, parents, route);
+            collectRoutes(parent, parents, route);
             return;
         }
 
@@ -180,10 +174,13 @@ public class Main
         if (route.size() == routes.get(0).size())
         {
             int newSteep = steep(route);
-            int currentSteep = routes.get(0).steep;
+            if (!COLLECT_ALL_LONGEST_ROUTES)
+            {
+                int currentSteep = routes.get(0).steep;
 
-            if (newSteep < currentSteep) { return; }
-            if (newSteep > currentSteep) { routes.clear(); }
+                if (newSteep < currentSteep) { return; }
+                if (newSteep > currentSteep) { routes.clear(); }
+            }
             routes.add(new Route(newSteep, route));
         }
     }
@@ -203,7 +200,7 @@ public class Main
         Node[][] graph = {};
 
         try (
-            BufferedReader reader = Files.newBufferedReader(file, Charset.forName("UTF-8"))
+                BufferedReader reader = Files.newBufferedReader(file, Charset.forName("UTF-8"))
         )
         {
             boolean isFirstLine = true;
@@ -232,5 +229,77 @@ public class Main
         }
 
         return graph;
+    }
+
+    private void addNeighbours(Node[][] graph)
+    {
+        for (Node[] nodes : graph)
+        {
+            for (Node node : nodes)
+            {
+                LinkedList<Node> neighbors = new LinkedList<>();
+                boolean isSource = true;
+
+                boolean west = node.col - 1 >= 0;
+                if (west)
+                {
+                    if (graph[node.row][node.col - 1].value < node.value)
+                    { neighbors.add(graph[node.row][node.col - 1]); }
+                    else
+                    { isSource = false; }
+                }
+
+                boolean south = node.row + 1 < graph.length;
+                if (south)
+                {
+                    if (graph[node.row + 1][node.col].value < node.value)
+                    { neighbors.add(graph[node.row + 1][node.col]); }
+                    else
+                    { isSource = false; }
+                }
+
+                boolean east = node.col + 1 < graph[node.row].length;
+                if (east)
+                {
+                    if (graph[node.row][node.col + 1].value < node.value)
+                    { neighbors.add(graph[node.row][node.col + 1]); }
+                    else
+                    { isSource = false; }
+                }
+
+                boolean north = node.row - 1 >= 0;
+                if (north)
+                {
+                    if (graph[node.row - 1][node.col].value < node.value)
+                    { neighbors.add(graph[node.row - 1][node.col]); }
+                    else
+                    { isSource = false; }
+                }
+
+                node.neighbours = neighbors;
+                node.isSource = isSource;
+            }
+        }
+    }
+
+    private static void print(List<Route> routes)
+    {
+        System.out.printf(
+                "\nfound %d route(s) with length %d:\n\n",
+                routes.size(), routes.get(0).size()
+        );
+        for (Route route : routes)
+        {
+            System.out.printf(
+                    "steep %d:\t%s\n",
+                    route.steep,
+                    route.stream()
+                            .map(n -> n.value)
+                            .sorted(Collections.reverseOrder())
+                            .map(Object::toString)
+                            .collect(Collectors.joining(" -> "))
+            );
+        }
+        System.out.println();
     }
 }
